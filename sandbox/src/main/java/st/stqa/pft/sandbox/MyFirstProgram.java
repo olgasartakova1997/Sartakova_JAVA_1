855337eba17601cea26ecfa815ddf44a262f39ef
osartakova <sartakowa.olya@yandex.ru>
2019-10-17 21:16:09 +0700

    Доработанное Задание №2

--- sandbox/src/main/java/st/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/st/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,13 +3,10 @@ package st.stqa.pft.sandbox;
 public class MyFirstProgram
 {
   public static void main(String[] args) {
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 5;
-    p1.y = 8;
-    p2.x = 5;
-    p2.y = 6;
+    Point p1 = new Point(7,8);
+    Point p2 = new Point(5,6);
     System.out.println("Расстояние между двумя точками = " + distance(p1,p2));
+    System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
   }
   public static double distance(Point p1, Point p2){
     return Math.sqrt(Math.pow(p2.x - p1.x, 2) + (Math.pow(p2.y - p1.y, 2)));
--- sandbox/src/main/java/st/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/st/stqa/pft/sandbox/Point.java
@@ -1,5 +1,15 @@
 package st.stqa.pft.sandbox;
 
 public class Point {
-  int x, y;
+  public double x;
+  public double y;
+
+ public Point(double x, double y) {
+ this.x = x;
+ this.y = y;
+ }
+
+ public double distance(Point p){
+   return Math.sqrt(Math.pow((this.x - p.x), 2) + (Math.pow((this.y - p.y), 2)));
+  }
 }
