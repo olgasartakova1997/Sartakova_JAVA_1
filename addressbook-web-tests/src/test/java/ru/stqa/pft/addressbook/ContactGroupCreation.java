0786e17eaaef4d2327272434b2435212226817b5
osartakova <sartakowa.olya@yandex.ru>
2019-10-27 20:26:39 +0700

    Задание №4_Исправленное

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactGroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactGroupCreation.java
@@ -36,7 +36,7 @@ public class ContactGroupCreation {
   }
 
   private void EnterContactCreation() {
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+    wd.findElement(By.xpath("(.//input[@name='submit'])[2]")).click();
   }
 
   private void fillContactForm(GroupDataSart groupDataSart) {
