c34df2b20c667824fea2c0f8e7b3b1f9b572fdc9
osartakova <sartakowa.olya@yandex.ru>
2019-11-18 23:58:43 +0700

    4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests2.java
@@ -4,16 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTests2 extends TestBase {
 @Test
 public void testGroupCreation() throws Exception {
-        app.getNavigationHelper().gotoGroupPage();
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("sart1", null, null));
-        app.getSessionHelper().logout();
-        List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size()+1);
-        }
+  app.getNavigationHelper().gotoGroupPage();
+  List<GroupData> before = app.getGroupHelper().getGroupList();
+  GroupData group = new GroupData("sart2", null, null);
+  app.getGroupHelper().fillGroupForm(group);
+  app.getSessionHelper().logout();
+  List<GroupData> after = app.getGroupHelper().getGroupList();
+  Assert.assertEquals(after.size(), before.size() + 1);
+
+  before.add(group);
+  Comparator<? super GroupData> byId = (g1, g2)->Integer.compare(g1.getId(),g2.getId());
+  before.sort(byId);
+  after.sort(byId);
+  Assert.assertEquals( before, after);
+ }
 }
