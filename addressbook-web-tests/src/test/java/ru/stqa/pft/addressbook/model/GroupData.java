048e3a49cfba1fe2ea1c878d5079226a0f73f0c7
osartakova <sartakowa.olya@yandex.ru>
2019-12-08 19:14:36 +0700

    Лекция 6.7. Формат JSON

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,48 +1,26 @@
-
 package ru.stqa.pft.addressbook.model;
-import java.util.Objects;
+
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+@XStreamAlias("group")
+
 public class GroupData {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
-  private  String name;
-  private  String header;
-  private  String footer;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
+  @Expose
+  private String name;
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
+  @Expose
+  private String header;
+
+  @Expose
+  private String footer;
 
   public int getId() {
-        return id;
-  }
-
-  public GroupData withId(int id) {
-    this.id = id;
-    return this;
-  }
-
-  public GroupData withName(String name) {
-    this.name = name;
-    return this;
-  }
-
-  public GroupData withHeader(String header) {
-    this.header = header;
-    return this;
-  }
-
-  public GroupData withFooter(String footer) {
-    this.footer = footer;
-    return this;
+    return id;
   }
 
   public String getName() {
@@ -57,6 +35,42 @@ public class GroupData {
     return footer;
   }
 
+  public GroupData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  public GroupData withFooter(String footer) {
+    this.footer = footer;
+    return this;
+  }
+
+  public GroupData withName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public GroupData withHeader(String header) {
+    this.header = header;
+    return this;
+  }
+
   @Override
   public String toString() {
     return "GroupData{" +
@@ -64,4 +78,4 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
-  }
+}
